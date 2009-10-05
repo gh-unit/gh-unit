efee86b82310f3aba8b1c2641d9d55251d8ba231
Gabriel Handford <gabrielh@gmail.com>
2009-10-04 18:00:47 -0700

    Updating test

--- Tests/GHTestMacrosTest.m
+++ Tests/GHTestMacrosTest.m
@@ -15,7 +15,13 @@
 @implementation GHTestMacrosTest
 
 - (void)testEquals {
-	GHAssertEqualsWithAccuracy(15, 15.000001, 0.001, nil);
+	GHAssertEqualsWithAccuracy(15.0, 15.000001, 0.001, nil);
+}
+
+- (void)testEqualsAccuracyMessage {
+	GHAssertThrows({
+		GHAssertEqualsWithAccuracy(15.0, 16.0, 0.001, nil);
+	}, nil);
 }
 
 - (void)testNSLog {
