97b90a11b3835bc07c141e63d07395b7cd4caf61
Gabriel Handford <gabrielh@gmail.com>
2009-10-19 19:32:17 -0700

    Empty test case.

---
+++ Tests/GHTestEmpty.m
@@ -0,0 +1,16 @@
+//
+//  GHTestEmpty.m
+//  GHUnitIPhone
+//
+//  Created by Gabriel Handford on 10/19/09.
+//  Copyright 2009 Yelp. All rights reserved.
+//
+
+#import "GHTestCase.h"
+
+@interface GHTestEmpty : GHTestCase { }
+@end
+
+@implementation GHTestEmpty
+
+@end
