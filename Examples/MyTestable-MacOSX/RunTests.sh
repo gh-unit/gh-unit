d00d8dc0897d66856a24c18e41d227cc9fab072f
Gabriel Handford <gabrielh@gmail.com>
2011-02-16 10:24:33 -0800

    Updating examples

--- Examples/MyTestable-MacOSX/RunTests.sh
+++ Examples/MyTestable-MacOSX/RunTests.sh
@@ -22,22 +22,23 @@ export NSHangOnUncaughtException=YES
 export NSAutoreleaseFreedObjectCheckEnabled=YES
 
 export DYLD_FRAMEWORK_PATH="$CONFIGURATION_BUILD_DIR"
-RUN_CMD="$TARGET_BUILD_DIR/$EXECUTABLE_PATH -RegisterForSystemEvents"
+RUN_CMD="$TARGET_BUILD_DIR/$EXECUTABLE_PATH"
 
 echo "Running: $RUN_CMD"
-$RUN_CMD
+$RUN_CMD -RegisterForSystemEvents
 RETVAL=$?
+
+unset DYLD_ROOT_PATH
+unset DYLD_FRAMEWORK_PATH
+unset IPHONE_SIMULATOR_ROOT
 
 if [ -n "$WRITE_JUNIT_XML" ]; then
   MY_TMPDIR=`/usr/bin/getconf DARWIN_USER_TEMP_DIR`
   RESULTS_DIR="${MY_TMPDIR}test-results"
 
   if [ -d "$RESULTS_DIR" ]; then
-	`$CP -r "$RESULTS_DIR" "$BUILD_DIR" && rm -r "$RESULTS_DIR"`
+	`cp -r "$RESULTS_DIR" "$BUILD_DIR" && rm -r "$RESULTS_DIR/"`
   fi
 fi
 
 exit $RETVAL
-	
-
-
