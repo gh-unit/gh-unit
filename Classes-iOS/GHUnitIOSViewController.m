695e65fb96ad62d2e280ecef1d38e24ad8e5ff0e
Paulo Oliveira <paulo@seqoy.com>
2011-03-26 16:51:03 -0300

    Conditionally use an custom Test Suite Class based on CLASS_SUITE environment variable.

--- Classes-iOS/GHUnitIOSViewController.m
+++ Classes-iOS/GHUnitIOSViewController.m
@@ -27,7 +27,8 @@
 //  OTHER DEALINGS IN THE SOFTWARE.
 //
 
-#import "GHUnitIOSViewController.h"
+#import "GHUnitIPhoneViewController.h"
+#import <objc/message.h>
 
 NSString *const GHUnitTextFilterKey = @"TextFilter";
 NSString *const GHUnitFilterKey = @"Filter";
@@ -89,12 +90,40 @@ NSString *const GHUnitFilterKey = @"Filter";
   [self reload];
 }
 
-- (GHUnitIOSTableViewDataSource *)dataSource {
-  if (!dataSource_) {
-    dataSource_ = [[GHUnitIOSTableViewDataSource alloc] initWithIdentifier:@"Tests" suite:[GHTestSuite suiteFromEnv]];  
-    [dataSource_ loadDefaults];    
-  }
-  return dataSource_;
+- (GHUnitIPhoneTableViewDataSource *)dataSource {
+ // Create Data Source if needed.
+ if (!dataSource_) {
+
+	 // Conditionally grab an custom Test Suite from CLASS_SUITE environment.
+	 const char* cClassSuite = getenv("CLASS_SUITE");
+	 if (cClassSuite) {
+		 NSString *name = [NSString stringWithUTF8String:cClassSuite];
+		 Class customClassSuite = NSClassFromString( name );
+
+		 ////////////////////////////////////	 ////////////////////////////////////
+		 // Init the suite.
+		 GHTestSuite *anSuite = [[[customClassSuite alloc] initWithName:name delegate:nil] autorelease];
+		 
+		 // If can't initialize, raise exception.
+		 if ( anSuite == nil ) {
+			 [NSException raise:NSInternalInconsistencyException
+						 format:@"The Test Suite Class '%@' defined in 'CLASS_SUITE' enviroment variable can't be initiated. Please check!", name];
+			 return;
+		 }
+		 
+		 ////////////////////////////////////
+		 // Init the Data Source.
+		 dataSource_ = [[GHUnitIPhoneTableViewDataSource alloc] initWithIdentifier:name suite:anSuite];  
+	 } 
+
+	 ////////////////////////////////////
+	 // Default Class Suite.
+	 else {
+         dataSource_ = [[GHUnitIOSTableViewDataSource alloc] initWithIdentifier:@"Tests" suite:[GHTestSuite suiteFromEnv]];
+	 }
+     [dataSource_ loadDefaults];
+ }
+ return dataSource_;
 }
 
 - (void)reload {
