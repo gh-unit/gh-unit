d8afa89583aee975c2989f88c4be642f759ec84e
Gabriel Handford <gabrielh@gmail.com>
2011-08-11 19:14:01 -0700

    Fixing app delegate bug when running on device

--- Examples/MyTestable-iOS/Tests/main.m
+++ Examples/MyTestable-iOS/Tests/main.m
@@ -11,7 +11,7 @@
 int main(int argc, char *argv[])
 {
   NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-  int retVal = UIApplicationMain(argc, argv, nil, @"GHUnitIPhoneAppDelegate");
+  int retVal = UIApplicationMain(argc, argv, nil, @"GHUnitIOSAppDelegate");
   [pool release];
   return retVal;
 }
