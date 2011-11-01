90ff69438eed173765a657d8bfa6cc4d0d21c68d
John Boiles <johnaboiles@gmail.com>
2011-10-31 17:19:06 -0700

    Cleaning up crufty stuff in iOS Example

--- Examples/MyTestable-iOS/MyTestable/MyTestableAppDelegate.h
+++ Examples/MyTestable-iOS/MyTestable/MyTestableAppDelegate.h
@@ -3,7 +3,7 @@
 //  MyTestable
 //
 //  Created by Gabriel Handford on 7/16/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
