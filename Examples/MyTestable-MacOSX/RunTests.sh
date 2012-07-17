4cdbcfe4c246471699a1bb9773447e71a4e3b0a0
Gabriel Handford <gabrielh@gmail.com>
2012-07-17 13:08:14 -0700

    Updating file

--- Examples/MyTestable-MacOSX/RunTests.sh
+++ Examples/MyTestable-MacOSX/RunTests.sh
@@ -8,6 +8,12 @@ fi
 export DYLD_ROOT_PATH="$SDKROOT"
 export DYLD_FRAMEWORK_PATH="$CONFIGURATION_BUILD_DIR"
 export IPHONE_SIMULATOR_ROOT="$SDKROOT"
+#export CFFIXED_USER_HOME="$TEMP_FILES_DIR/iPhone Simulator User Dir" # Be compatible with google-toolbox-for-mac
+
+#if [ -d $"CFFIXED_USER_HOME" ]; then
+#  rm -rf "$CFFIXED_USER_HOME"
+#fi
+#mkdir -p "$CFFIXED_USER_HOME"
 
 export NSDebugEnabled=YES
 export NSZombieEnabled=YES
@@ -30,11 +36,19 @@ if [ ! -e "$TEST_TARGET_EXECUTABLE_PATH" ]; then
   exit 1
 fi
 
+# If trapping fails, make sure we kill any running securityd
+launchctl list | grep GHUNIT_RunIPhoneSecurityd && launchctl remove GHUNIT_RunIPhoneSecurityd
+SCRIPTS_PATH=`cd $(dirname $0); pwd`
+launchctl submit -l GHUNIT_RunIPhoneSecurityd -- "$SCRIPTS_PATH"/RunIPhoneSecurityd.sh $IPHONE_SIMULATOR_ROOT $CFFIXED_USER_HOME
+trap "launchctl remove GHUNIT_RunIPhoneSecurityd" EXIT TERM INT
+
 RUN_CMD="\"$TEST_TARGET_EXECUTABLE_PATH\" -RegisterForSystemEvents"
 
 echo "Running: $RUN_CMD"
+set +o errexit # Disable exiting on error so script continues if tests fail
 eval $RUN_CMD
 RETVAL=$?
+set -o errexit
 
 unset DYLD_ROOT_PATH
 unset DYLD_FRAMEWORK_PATH
