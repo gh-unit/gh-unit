d10f5e6c753dce79c14c63546c408bee298bf566
Felix Schulze <fschulze@autoscout24.com>
2013-10-30 13:22:21 +0100

    Fix GHUNIT_CLI test run for iOS 6.1+ Fixes #96

--- Scripts/RunTests.sh
+++ Scripts/RunTests.sh
@@ -8,12 +8,12 @@ fi
 export DYLD_ROOT_PATH="$SDKROOT"
 export DYLD_FRAMEWORK_PATH="$CONFIGURATION_BUILD_DIR"
 export IPHONE_SIMULATOR_ROOT="$SDKROOT"
-#export CFFIXED_USER_HOME="$TEMP_FILES_DIR/iPhone Simulator User Dir" # Be compatible with google-toolbox-for-mac
+export CFFIXED_USER_HOME="$TEMP_FILES_DIR/iPhone Simulator User Dir" # Be compatible with google-toolbox-for-mac
 
-#if [ -d $"CFFIXED_USER_HOME" ]; then
-#  rm -rf "$CFFIXED_USER_HOME"
-#fi
-#mkdir -p "$CFFIXED_USER_HOME"
+if [ -d $"CFFIXED_USER_HOME" ]; then
+  rm -rf "$CFFIXED_USER_HOME"
+fi
+mkdir -p "$CFFIXED_USER_HOME"
 
 export NSDebugEnabled=YES
 export NSZombieEnabled=YES
@@ -37,10 +37,11 @@ if [ ! -e "$TEST_TARGET_EXECUTABLE_PATH" ]; then
 fi
 
 # If trapping fails, make sure we kill any running securityd
-launchctl list | grep GHUNIT_RunIPhoneSecurityd && launchctl remove GHUNIT_RunIPhoneSecurityd
-SCRIPTS_PATH=`cd $(dirname $0); pwd`
-launchctl submit -l GHUNIT_RunIPhoneSecurityd -- "$SCRIPTS_PATH"/RunIPhoneSecurityd.sh $IPHONE_SIMULATOR_ROOT $CFFIXED_USER_HOME
-trap "launchctl remove GHUNIT_RunIPhoneSecurityd" EXIT TERM INT
+# TODO: Can we remove that code? Why is it used?
+#launchctl list | grep GHUNIT_RunIPhoneSecurityd && launchctl remove GHUNIT_RunIPhoneSecurityd
+#SCRIPTS_PATH=`cd $(dirname $0); pwd`
+#launchctl submit -l GHUNIT_RunIPhoneSecurityd -- "$SCRIPTS_PATH"/RunIPhoneSecurityd.sh $IPHONE_SIMULATOR_ROOT $CFFIXED_USER_HOME
+#trap "launchctl remove GHUNIT_RunIPhoneSecurityd" EXIT TERM INT
 
 RUN_CMD="\"$TEST_TARGET_EXECUTABLE_PATH\" -RegisterForSystemEvents"
 
--- Tests/GHUnitIOSTestMain.m
+++ Tests/GHUnitIOSTestMain.m
@@ -27,12 +27,44 @@
 //  OTHER DEALINGS IN THE SOFTWARE.
 //
 
+#import <objc/runtime.h>
 
-#import <UIKit/UIKit.h>
+//see https://github.com/gh-unit/gh-unit/issues/96#issuecomment-12881447
+
+@interface UIWindow (Private)
+- (void) swizzled_createContext;
+@end
+
+@implementation UIWindow (Private)
+
+- (void) swizzled_createContext {
+    // Should not create a context
+}
+
+@end
 
 int main(int argc, char *argv[]) {
-  @autoreleasepool {
-    int retVal = UIApplicationMain(argc, argv, nil, @"GHUnitIOSAppDelegate");
+    int retVal;
+    @autoreleasepool {
+        
+        BOOL isCli = getenv("GHUNIT_CLI") ? YES : NO;
+        CFMessagePortRef portRef = NULL;
+        if (isCli == YES) {
+            
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wundeclared-selector"
+#pragma clang diagnostic ignored "-Wselector"
+            Method originalWindowCreateContext = class_getInstanceMethod([UIWindow class], @selector(_createContext));
+#pragma clang diagnostic pop
+            
+            Method swizzledWindowCreateConext = class_getInstanceMethod([UIWindow class], @selector(swizzled_createContext));
+            method_exchangeImplementations(originalWindowCreateContext, swizzledWindowCreateConext);
+            
+            portRef = CFMessagePortCreateLocal(NULL, (CFStringRef) @"PurpleWorkspacePort", NULL, NULL, NULL);
+        }
+        
+        retVal = UIApplicationMain(argc, argv, NSStringFromClass([UIApplication class]), @"GHUnitIOSAppDelegate");
+        if (portRef != NULL) { CFRelease(portRef); }
+    }
     return retVal;
-  }
 }
